eeb51b3352939a184c13b9a4be0275bac57eda83
xF0RTUNAx <x8r8x54n9r@privaterelay.appleid.com>
2026-06-20 05:55:38 +0300

    Update Supabase anon key in config.js

--- fortuna-game/js/config.js
+++ fortuna-game/js/config.js
@@ -5,7 +5,7 @@
 
 const CONFIG = {
   SUPABASE_URL: "https://rdxoaalzlwbyjxebxtyo.supabase.co",
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJkeG9hYWx6bHdieWp4ZWJ4dHlvIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4MTkwMDUzOSwiZXhwIjoyMDk3NDc2NTM5fQ.Ww6bHb4_MDNfzilfz2lTTqI9j8Ucrf4PZfRIubfisyg",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJkeG9hYWx6bHdieWp4ZWJ4dHlvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE5MDA1MzksImV4cCI6MjA5NzQ3NjUzOX0.tRaxzqPoXIvouqoCnT5DTCbSA17mrglU4fuL-bTCRrY",
 
   // Edge Function входа по логину/паролю:
   AUTH_PASSWORD_URL:
